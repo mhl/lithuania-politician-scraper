b1433451eff6e1abb6d908ef3e6c789498b1852a
Mark Longair <mhl@pobox.com>
2015-08-07 15:00:31 +0100

    Add a basic scraper for the couchdb.manoseimas.lt MP data

--- scraper.py
+++ scraper.py
@@ -1,24 +1,57 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import json
+import requests
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+import scraperwiki
+
+r = requests.get('http://couchdb.manoseimas.lt/mps/_design/mps-by-start-date/_view/MPs%20by%20start%20date')
+
+data = r.json()
+
+results = []
+
+def get_memberships(value, membership_type):
+    return [
+        g for g in value['groups']
+        if g['type'] == membership_type
+    ]
+
+class NoGroupFound(Exception):
+    pass
+
+def get_membership_one_expected(value, membership_type):
+    results = get_memberships(value, membership_type)
+    if not results:
+        print "Warning: no group found"
+        raise NoGroupFound()
+    elif len(results) > 1:
+        raise Exception, "Found {count} {mtype} groups in {value}".format(
+            count=len(results),
+            mtype=membership_type,
+            value=json.dumps(value, indent=4, sort_keys=True)
+        )
+    return results[0]
+
+for row in data['rows']:
+    value = row['value']
+    full_name = u'{0} {1}'.format(value['first_name'], value['last_name'])
+
+    parl_membership = get_membership_one_expected(value, 'parliament')
+    # Some people seem to be missing a party field:
+    try:
+        party_membership = get_membership_one_expected(value, 'party')
+    except NoGroupFound:
+        party_membership = {}
+    start_date, end_date = parl_membership['membership']
+    person_data = (
+        {
+            'id': value['source']['id'],
+            'name': full_name,
+            'area': value['constituency'],
+            'group': party_membership.get('name'),
+            'start_date': start_date,
+            'end_date': end_date,
+        }
+    )
+    scraperwiki.sqlite.save(unique_keys=['id'], data=person_data)
