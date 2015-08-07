0fb8acb7981b387f485a6e9557a78afc431ee2a6
Mark Longair <mhl@pobox.com>
2015-08-07 15:28:37 +0100

    Only get members of the current parliament
    Since some are missing from the previous term

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from datetime import date, datetime
 import json
 import requests
 
@@ -10,6 +11,17 @@ r = requests.get('http://couchdb.manoseimas.lt/mps/_design/mps-by-start-date/_vi
 data = r.json()
 
 results = []
+
+terms = {
+    '10': {
+        'start_date': date(2008, 11, 17),
+        'end_date': date(2012, 11, 16),
+    },
+    '11': {
+        'start_date': date(2012, 11, 17),
+        'end_date': None,
+    }
+}
 
 def get_memberships(value, membership_type):
     return [
@@ -33,6 +45,13 @@ def get_membership_one_expected(value, membership_type):
         )
     return results[0]
 
+def get_term(start_date, end_date):
+    '''For the moment, only return a term for those in the current parliament'''
+    if not end_date:
+        return '11'
+    if end_date >= terms['11']['start_date']:
+        return '11'
+
 for row in data['rows']:
     value = row['value']
     full_name = u'{0} {1}'.format(value['first_name'], value['last_name'])
@@ -43,7 +62,14 @@ for row in data['rows']:
         party_membership = get_membership_one_expected(value, 'party')
     except NoGroupFound:
         party_membership = {}
-    start_date, end_date = parl_membership['membership']
+    start_date, end_date = [
+        None if s is None else datetime.strptime(s, '%Y-%m-%d').date()
+        for s in parl_membership['membership']
+    ]
+    term = get_term(start_date, end_date)
+    if not term:
+        continue
+    start_date = max(start_date, terms['11']['start_date'])
     person_data = (
         {
             'id': value['source']['id'],
